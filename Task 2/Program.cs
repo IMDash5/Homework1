45082f5377567fe7db0e75b3b4cb63823c9dc58b
Ivan <mmavrin80@gmail.com>
2022-03-18 21:51:48 +0300

    Task 2

---
+++ Task 2/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.Write("Введите первое число: ");
+string num1 = Console.ReadLine();
+int number1 = int.Parse (num1); 
+
+Console.Write("Введите второе число: ");
+string num2 = Console.ReadLine();
+int number2 = int.Parse (num2);
+
+Console.Write("Введите третье число: ");
+string num3 = Console.ReadLine();
+int number3 = int.Parse (num3);
+
+int max = 0;
+int min = 0;
+
+if (number1 > number2) 
+{
+    max = number1;
+    min = number2;
+}
+else 
+{
+    max = number2;
+    min = number1;
+}
+if (max < number3)
+{
+    max = number3;
+}
+else 
+{
+    if (min > number3)
+    {
+        min = number3;
+    }
+}
+Console.WriteLine($"Максимальное число: {max}. Минимальное число: {min}.");
